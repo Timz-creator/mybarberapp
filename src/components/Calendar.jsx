d4a63bd5487c8e7baa0cd221c8ecdea150f97e0e
Timz-creator <timarleyf@gmail.com>
2023-05-05 19:34:38 +0100

    added an alert when submit is pressed

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -5,31 +5,38 @@ import {
 } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 
-
-
-
 const Calendar = () => {
-
   const getValue = (event) => {
-    console.log('Event: ', event.target.value);
-  }
+    console.log("Event: ", event.target.value);
+  };
+  const confirmation = () => {
+    let name = document.getElementById("nameInput").value;
+    let barbers = document.getElementById('barbers').value
+    alert(`Appointment confirmed with ${barbers}! See you soon ${name}`);
+  };
 
   return (
     <div className="flex items-center justify-center">
       <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <div className="grid space-y-20">
-          <StaticDateTimePicker />
-          <h1>Barbers</h1>
-          <select>
+        <div className="grid space-y-5">
+          <StaticDateTimePicker onChange={(date) => console.log(date)} />
+          <select id='barbers'>
             <option>Dan</option>
             <option>Ty</option>
             <option>Reece</option>
             <option>Conor</option>
             <option>Nate</option>
           </select>
-          <input placeholder="Name" onChange= {getValue} required />
-          <input placeholder="Email" required/>
-          <button className="bg-white">Submit</button>
+          <input
+            id="nameInput"
+            placeholder="Name"
+            onChange={getValue}
+            required
+          />
+          <input placeholder="Email" required />
+          <button className="bg-white" onClick={confirmation}>
+            Submit
+          </button>
         </div>
       </LocalizationProvider>
     </div>
