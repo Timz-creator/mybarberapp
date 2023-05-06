f3477e0074d0ca9721e3c8560e63f8ee5808e97d
Timz-creator <timarleyf@gmail.com>
2023-05-06 21:01:22 +0100

    including the date in the alert

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -4,23 +4,30 @@ import {
   StaticDateTimePicker,
 } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { useState } from "react";
 
 const Calendar = () => {
+  const [date, setDate] = useState(null)
   const getValue = (event) => {
     console.log("Event: ", event.target.value);
   };
   const confirmation = () => {
     let name = document.getElementById("nameInput").value;
-    let barbers = document.getElementById('barbers').value
-    alert(`Appointment confirmed with ${barbers}! See you soon ${name}`);
+    let barbers = document.getElementById("barbers").value;
+    alert(
+      `Ypur Appointment at ${date} with ${barbers} is confirmed! See you soon ${name}`
+    );
   };
 
   return (
     <div className="flex items-center justify-center">
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <div className="grid space-y-5">
-          <StaticDateTimePicker onChange={(date) => console.log(date)} />
-          <select id='barbers'>
+          <StaticDateTimePicker
+            id="appointment"
+            onChange={(date) => setDate(date)}
+          />
+          <select id="barbers">
             <option>Dan</option>
             <option>Ty</option>
             <option>Reece</option>
